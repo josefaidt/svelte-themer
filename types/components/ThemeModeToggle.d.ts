040091798bf86c7b81cffe297a6800e87d2a0c01
josef <josef.aidt@gmail.com>
2021-10-29 17:26:47 -0500

    chore: remove old type files

--- types/components/ThemeModeToggle.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="svelte" />
-import { SvelteComponent } from "svelte";
-
-export interface ThemeModeToggleProps {}
-
-export default class ThemeModeToggle extends SvelteComponent<ThemeModeToggleProps, {}, {}> {}
