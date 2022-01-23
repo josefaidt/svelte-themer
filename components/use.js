3f03a12c90a58d6ce8d03dd881742139db33a2ac
josefaidt <josef.aidt@gmail.com>
2022-01-23 16:42:05 -0600

    feat: separate inline vs stylesheet actions

--- components/use.js
+++ components/use.js
@@ -7,12 +7,49 @@ import { createCSSVariableCollection } from '../support/css'
  */
 
 /**
+ *
+ * @param {HTMLElement} node
+ * @param {Object.<string, string|number>} theme
+ * @returns
+ */
+export async function theme(node, theme) {
+  /**
+   *
+   * @param {string} name
+   * @param {string} value
+   * @returns {void}
+   */
+  function setProperty(name, value) {
+    if (!node.style && node.document?.documentElement) {
+      node.document.documentElement.style.setProperty(name, value)
+      return
+    }
+    node.style.setProperty(name, value)
+    return
+  }
+
+  function setProperties() {
+    const variables = createCSSVariableCollection(theme)
+    for (let [name, value] of variables) {
+      setProperty(name, value)
+    }
+  }
+
+  return {
+    update(newTheme) {
+      theme = newTheme
+      setProperties()
+    },
+  }
+}
+
+/**
  * use:theme
  * @param {HTMLElement} node
  * @param {Object.<string, string|number>} theme
  * @returns {ActionReturn}
  */
-export async function theme(node, theme) {
+export async function stylesheet(node, theme) {
   const stylesheet = document.createElement('style')
 
   function setProperties() {
