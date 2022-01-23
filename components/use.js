60b5b3c1e5611835a49c8e051f609b888a7e8c09
josefaidt <josef.aidt@gmail.com>
2022-01-23 16:03:09 -0600

    feat: theme action to use stylesheet

--- components/use.js
+++ components/use.js
@@ -13,27 +13,20 @@ import { createCSSVariableCollection } from '../support/css'
  * @returns {ActionReturn}
  */
 export async function theme(node, theme) {
-  /**
-   * setProperty helper with `window` support
-   * @param {string} name
-   * @param {string} value
-   * @returns {void}
-   */
-  function setProperty(name, value) {
-    if (!node.style && node.document?.documentElement) {
-      node.document.documentElement.style.setProperty(name, value)
-      return
-    }
-    node.style.setProperty(name, value)
-    return
-  }
+  const stylesheet = document.createElement('style')
 
   function setProperties() {
     const variables = createCSSVariableCollection(theme)
+    let innerHTML = `:root{`
     for (let [name, value] of variables) {
-      setProperty(name, value)
+      innerHTML += `${name}:${value};`
     }
+    innerHTML += '}'
+    stylesheet.innerHTML = innerHTML
+    node.prepend(stylesheet)
   }
+
+  setProperties()
 
   return {
     update(newTheme) {
